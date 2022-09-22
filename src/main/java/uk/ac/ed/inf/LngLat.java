dd00f803a01fdd678a4eed4c4d25cc205ea65b48
AndreasHiropedi <s2015345@ed.ac.uk>
2022-09-22 17:03:25 +0100

    few changes made

---
+++ src/main/java/uk/ac/ed/inf/LngLat.java
@@ -0,0 +1,26 @@
+package uk.ac.ed.inf;
+
+public record LngLat(double lng, double lat)
+{
+    public boolean inCentralArea()
+    {
+        // ToDo
+        return true;
+    }
+
+    public double distanceTo(LngLat location)
+    {
+        return Math.sqrt( Math.pow( (this.lng - location.lng) , 2) - Math.pow( (this.lat - location.lat) , 2) );
+    }
+
+    public boolean closeTo(LngLat location)
+    {
+        return distanceTo(location) <= 0.00015;
+    }
+
+    public LngLat nextPosition(CompassLocation direction)
+    {
+        // ToDo
+        return null;
+    }
+}
