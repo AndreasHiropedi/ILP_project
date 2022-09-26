2d83cc282a6b48e6d79b322e06508fd214d0dc65
AndreasHiropedi <s2015345@ed.ac.uk>
2022-09-26 17:05:32 +0100

    finished centralArea

--- src/main/java/uk/ac/ed/inf/LngLat.java
+++ src/main/java/uk/ac/ed/inf/LngLat.java
@@ -3,7 +3,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -27,10 +26,19 @@ public record LngLat(
     public boolean inCentralArea() throws IOException {
         String baseURL = "https://ilp-rest.azurewebsites.net/centralArea";
         ArrayList<LngLat> corners = new ObjectMapper().readValue(new URL(baseURL), new TypeReference<>(){});
-        boolean isInArea = true;
 
-
-        return isInArea;
+        int a, b;
+        boolean isInsideArea = false;
+        for (a = 0, b = corners.size() - 1; a < corners.size(); b = a++)
+        {
+            if ((corners.get(a).lat >= this.lat) != (corners.get(b).lat >= this.lat)
+                    && (this.lng < (corners.get(b).lng - corners.get(a).lng) * (this.lat - corners.get(a).lat) /
+                    (corners.get(b).lat - corners.get(a).lat) + corners.get(a).lng))
+            {
+                isInsideArea = !isInsideArea;
+            }
+        }
+        return isInsideArea;
     }
 
     /**
@@ -57,7 +65,7 @@ public record LngLat(
      */
     public LngLat nextPosition(CompassLocation position)
     {
-        // TODO
+
         return null;
     }
 
--- src/main/java/uk/ac/ed/inf/RetrieveData.java
+++ src/main/java/uk/ac/ed/inf/RetrieveData.java
@@ -1,5 +1,7 @@
 package uk.ac.ed.inf;
 
+import java.util.ArrayList;
+
 public class RetrieveData
 {
 
