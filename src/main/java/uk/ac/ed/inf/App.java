ff9b116d91448d48627d3c8f47a9e575b37363ca
AndreasHiropedi <s2015345@ed.ac.uk>
2022-10-07 12:15:53 +0100

    bullshit

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,14 +1,9 @@
 package uk.ac.ed.inf;
 
-import java.io.IOException;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args ) throws IOException {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args )
+    {
+
     }
 }
