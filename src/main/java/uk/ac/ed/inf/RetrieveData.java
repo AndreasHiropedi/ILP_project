12a64277ed4c4af77acc5790c24b58e9e8170586
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-10-12 16:35:46 +0100

    Update RetrieveData.java

--- src/main/java/uk/ac/ed/inf/RetrieveData.java
+++ src/main/java/uk/ac/ed/inf/RetrieveData.java
@@ -3,115 +3,28 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 
 /**
- * this singleton class is used to retrieve data
+ * this class is used to retrieve data
  * from the REST server
  */
 public class RetrieveData
 {
-    private static RetrieveData retrieveData;
-    private static final String baseURL = "https://ilp-rest.azurewebsites.net";
-
-    public RetrieveData()
-    {
-
-    }
-
     /**
-     * creates an instance of the singleton class
-     * @return the instance of the singleton object
+     * generic method for retrieving the data from the REST server
+     * for each individual class
+     * @param baseURL the base REST server
+     * @param extension the necessary extension (depends on the class)
+     * @param typeReference the type of the object (or list of objects) to be returned
+     * @param <T> allows for multiple classes to use this method
+     * @return the data from the REST server in the specified format
      */
-    public static RetrieveData getInstance()
-    {
-        if (retrieveData == null)
-        {
-            retrieveData = new RetrieveData();
-        }
-        return retrieveData;
-    }
-
-    /**
-     *
-     * @param baseURL
-     * @param className
-     * @return
-     * @param <T>
-     */
-    public<T> ArrayList<T> getData(URL baseURL, Class<T> className)
-    {
-        String extension;
-        switch (className.toString())
-        {
-            case "LngLat":
-                extension = "/centralArea";
-            case "Restaurant":
-                extension = "/restaurants";
-            case "Order":
-                extension = "/orders";
-            default:
-                extension = "";
-        }
-        try
-        {
-            return new ObjectMapper().readValue(new URL(baseURL + extension), new TypeReference<>(){});
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * retrieves the data for the Central Campus Area
-     * from the REST server
-     * @return an arraylist of the co-ordinates of the
-     * Central Campus Area
-     */
-    public ArrayList<LngLat> retrieveCentralArea()
+    public static <T> T getData(URL baseURL, String extension, TypeReference<T> typeReference)
     {
         try
         {
-            return new ObjectMapper().readValue(new URL(baseURL + "/centralArea"), new TypeReference<>(){});
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * retrieves the data for all restaurants on the PizzaDronz
-     * app from the REST server
-     * @param baseURL the base URL which stores all restaurant data
-     *                on the REST server
-     * @return an arraylist of all the details for all
-     * restaurants
-     */
-    public ArrayList<Restaurant> retrieveRestaurantData(URL baseURL)
-    {
-        try
-        {
-            return new ObjectMapper().readValue(new URL(baseURL.toString() + "/restaurants"), new TypeReference<>(){});
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * retrieves the data for all orders made on the
-     * PizzaDronz app from the REST server
-     * @return an arraylist of all the details for all
-     * orders placed
-     */
-    public ArrayList<Order> retrieveOrderData()
-    {
-        try
-        {
-            return new ObjectMapper().readValue(new URL(baseURL + "/orders"), new TypeReference<>(){});
+            return new ObjectMapper().readValue(new URL(baseURL.toString() + extension),
+                    typeReference);
         }
         catch (IOException e)
         {
