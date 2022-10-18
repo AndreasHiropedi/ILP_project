4aab7cc5a9ee539c2f1abad20e1b63cba642db68
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-10-18 12:52:27 +0100

    Add files via upload

---
+++ src/main/java/uk/ac/ed/inf/FlightPath.java
@@ -0,0 +1,9 @@
+package uk.ac.ed.inf;
+
+/**
+ *
+ */
+public class FlightPath
+{
+
+}
---
+++ src/main/java/uk/ac/ed/inf/OrderOutcome.java
@@ -0,0 +1,11 @@
+package uk.ac.ed.inf;
+
+/**
+ * this enum is used to store details regarding each
+ * order outcome (whether it was delivered, valid but not delivered,
+ * invalid, etc.)
+ */
+public enum OrderOutcome
+{
+
+}
