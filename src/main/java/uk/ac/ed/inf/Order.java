52b49635cc43407b394b265d6e9356f9d75051f4
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-11-20 17:50:22 +0000

    Add files via upload

--- src/main/java/uk/ac/ed/inf/Order.java
+++ src/main/java/uk/ac/ed/inf/Order.java
@@ -1,5 +1,6 @@
 package uk.ac.ed.inf;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import exceptions.InvalidPizzaCombinationException;
 
 import java.util.ArrayList;
@@ -8,31 +9,61 @@ import java.util.List;
 
 
 /**
- * this class is used to store all orders based on the
- * information from the REST server
- * @param orderNo the unique number used to identify an order
- * @param orderDate the date the order was created/ placed
- * @param customer the name of the person (customer) who made the order
- * @param creditCardNumber the credit card number the customer
- *                         used to pay for the order
- * @param creditCardExpiry the expiry date of the credit card
- *                         the customer used to pay for the order
- * @param cvv the cvv security code number on the credit card
- *            the customer used to pay for the order
- * @param priceTotalInPence the total price of all the items included
- *                          in the order
- * @param orderItems a list of all the items included in the order
+ * this class is used to store all the information about orders
+ * from the REST server
  */
-public record Order(
-        String orderNo,
-        String orderDate,
-        String customer,
-        String creditCardNumber,
-        String creditCardExpiry,
-        String cvv,
-        int priceTotalInPence,
-        List<String> orderItems)
+public class Order
 {
+    @JsonProperty("orderNo")
+    private String orderNo;
+    @JsonProperty("orderDate")
+    private String orderDate;
+    @JsonProperty("customer")
+    private String customer;
+    @JsonProperty("creditCardNumber")
+    private String creditCardNumber;
+    @JsonProperty("creditCardExpiry")
+    private String creditCardExpiry;
+    @JsonProperty("cvv")
+    private String cvv;
+    @JsonProperty("priceTotalInPence")
+    private int priceTotalInPence;
+    @JsonProperty("orderItems")
+    private List<String> orderItems;
+
+    // this field is used to filter out invalid orders
+    private OrderOutcome outcome;
+
+    /**
+     * constructor for the order objects
+     * @param orderNo: the unique number used to identify an order
+     * @param orderDate: the date the order was created/ placed
+     * @param customer: the name of the person (customer) who made the order
+     * @param creditCardNumber: the credit card number the customer
+     *                         used to pay for the order
+     * @param creditCardExpiry: the expiry date of the credit card
+     *                         the customer used to pay for the order
+     * @param cvv: the cvv security code number on the credit card
+     *            the customer used to pay for the order
+     * @param priceTotalInPence: the total price of all the items included
+     *                          in the order
+     * @param orderItems: a list of all the items included in the order
+     */
+    public Order(String orderNo, String orderDate, String customer,
+                 String creditCardNumber, String creditCardExpiry,
+                 String cvv, int priceTotalInPence, List<String> orderItems)
+    {
+        this.orderNo = orderNo;
+        this.orderDate = orderDate;
+        this.customer = customer;
+        this.creditCardNumber = creditCardNumber;
+        this.creditCardExpiry = creditCardExpiry;
+        this.cvv = cvv;
+        this.priceTotalInPence = priceTotalInPence;
+        this.orderItems = orderItems;
+    }
+
+    
 
     /**
      * computes the cost, in pence, of all items selected
