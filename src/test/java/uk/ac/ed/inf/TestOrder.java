ddecad6132c1eb5d218b21182fc57afd32781994
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-12-02 15:23:32 +0000

    Add files via upload

--- src/test/java/uk/ac/ed/inf/TestOrder.java
+++ src/test/java/uk/ac/ed/inf/TestOrder.java
@@ -1,7 +1,6 @@
 package uk.ac.ed.inf;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import exceptions.InvalidPizzaCombinationException;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,8 +9,6 @@ import org.junit.jupiter.api.TestInfo;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,21 +26,20 @@ public class TestOrder
         System.out.println(testInfo.getDisplayName());
     }
 
-    ArrayList<Order> orders = RetrieveData.getData(new URL("https://ilp-rest.azurewebsites.net"), "/orders", new TypeReference<>(){});
-    Order order1 = orders.get(0);
-    Order order2 = orders.get(2);
-    Order order3 = orders.get(11);
-    Menu item = new Menu("Joe's special pizza", 55000);
-    List<Menu> menuTests = Arrays.stream(new Menu[]{item}).toList();
-    Restaurant restaurants1 = new Restaurant("Johnny's", menuTests, -3.82, 55.67);
+
 
     @Test
     @DisplayName("Testing different scenarios for getDeliveryCost method")
-    public void testGetDeliveryCost() throws InvalidPizzaCombinationException
-    {
-        assertEquals(2600, order1.getDeliveryCost());
-        assertEquals(2400, order2.getDeliveryCost());
-        assertEquals(2500, order3.getDeliveryCost());
+    public void testGetDeliveryCost() throws MalformedURLException {
+        Restaurant[] restaurants = Restaurant.getRestaurantsFromRestServer(new URL("https://ilp-rest.azurewebsites.net"));
+        Order.setRestaurants(restaurants);
+        ArrayList<Order> orders = RetrieveData.getData(new URL("https://ilp-rest.azurewebsites.net"), "/orders", new TypeReference<>(){});
+        Order order1 = orders.get(0);
+        Order order2 = orders.get(2);
+        Order order3 = orders.get(11);
+        assertEquals(2400, order1.getDeliveryCost());
+        assertEquals(2500, order2.getDeliveryCost());
+        assertEquals(100, order3.getDeliveryCost());
         System.out.println("GetDeliveryCost method works as expected!");
     }
 
