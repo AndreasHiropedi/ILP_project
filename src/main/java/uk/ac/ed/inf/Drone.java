d939a813835b3e27a180e7dd042464e018f11d54
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-12-02 18:26:25 +0000

    Add files via upload

--- src/main/java/uk/ac/ed/inf/Drone.java
+++ src/main/java/uk/ac/ed/inf/Drone.java
@@ -1,9 +1,22 @@
 package uk.ac.ed.inf;
 
 /**
- *
+ * this class is used to monitor the movement of the drone
+ * when delivering orders and computing flight paths
  */
 public class Drone
 {
+    // this field stores the remaining number of moves
+    // that the drone has before it runs out of battery
+    private int numberOfMoves;
+
+    /**
+     * constructor method for the drone object
+     */
+    public Drone()
+    {
+        int numberOfMoves = 2000;
+    }
+
 
 }
---
+++ src/main/java/uk/ac/ed/inf/GeoJSONWriter.java
@@ -0,0 +1,19 @@
+package uk.ac.ed.inf;
+
+/**
+ * this class is used to write the GeoJSON files
+ * needed as part of the specification
+ */
+public class GeoJSONWriter
+{
+
+    /**
+     * constructor method for the GeoJSON file
+     * writer
+     */
+    public GeoJSONWriter()
+    {
+
+    }
+
+}
