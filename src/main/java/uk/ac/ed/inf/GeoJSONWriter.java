a44aa3a65bededb4ef7a12b1108ca389ba6c19ba
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-12-07 11:53:34 +0000

    Add files via upload

--- src/main/java/uk/ac/ed/inf/GeoJSONWriter.java
+++ src/main/java/uk/ac/ed/inf/GeoJSONWriter.java
@@ -1,5 +1,15 @@
 package uk.ac.ed.inf;
 
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.Point;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * this class is used to write the GeoJSON files
  * needed as part of the specification
@@ -14,6 +24,43 @@ public class GeoJSONWriter
     public GeoJSONWriter()
     {
 
+    }
+
+    /**
+     * this method writes the data for a given drone flight plan
+     * for delivering orders for a given date (from command line)
+     * to the appropriate GeoJSON file
+     * @param drone the drone object containing the flight plan
+     *              to be written
+     */
+    public void writePlanToGeoJSON(Drone drone)
+    {
+        // the directory path to the resultfiles folder
+        String path = System.getProperty("user.dir") + "/resultfiles";
+        // get all the necessary information from the drone object
+        List<LngLat> finalCoordinates = drone.getAllMoves();
+        String fileDate = drone.getDateOfFlightPlan();
+        try
+        {
+            // set up all the data for GeoJSON format
+            ArrayList<Point> pointsList = new ArrayList<>();
+            for (LngLat point : finalCoordinates) {
+                pointsList.add(Point.fromLngLat(point.lng(), point.lat()));
+            }
+            LineString line = LineString.fromLngLats(pointsList);
+            Feature feature = Feature.fromGeometry(line);
+            FeatureCollection collection = FeatureCollection.fromFeature(feature);
+            String fileName = "drone-" + fileDate + ".geojson";
+            // and write it to the appropriate file
+            FileWriter file = new FileWriter(path + "/" + fileName);
+            file.write(collection.toJson());
+            file.close();
+        }
+        catch (IOException e)
+        {
+            System.err.println("Exception occurred: \n" + e);
+            System.exit(1);
+        }
     }
 
 }
