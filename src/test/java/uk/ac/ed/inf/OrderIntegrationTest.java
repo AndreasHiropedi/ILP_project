58af94a4139d2c9ae4b14f2b0a30ad13f445f854
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-12-29 16:46:22 +0200

    Add files via upload

--- src/test/java/uk/ac/ed/inf/OrderIntegrationTest.java
+++ src/test/java/uk/ac/ed/inf/OrderIntegrationTest.java
@@ -1,9 +1,15 @@
 package uk.ac.ed.inf;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -13,10 +19,153 @@ import static org.junit.jupiter.api.Assertions.*;
 public class OrderIntegrationTest
 {
 
+    // =========================================================================
+    // ============================= CONSTRUCTOR ===============================
+    // =========================================================================
+
+    /**
+     * constructor method (used to also handle URL exceptions)
+     * @throws MalformedURLException error thrown when an invalid URL is used
+     */
+    public OrderIntegrationTest() throws MalformedURLException
+    {
+
+    }
+
+    // =========================================================================
+    // ================================ TESTS ==================================
+    // =========================================================================
+
     @BeforeEach
     void displayTestName(TestInfo testInfo)
     {
         System.out.println(testInfo.getDisplayName());
+    }
+
+    // base URL for REST server
+    URL baseUrl = new URL("https://ilp-rest.azurewebsites.net");
+
+    @Test
+    @DisplayName("Testing if order validation for REST server data for all orders works as expected")
+    void testOrderValidationForAllOrders()
+    {
+        // set up data before checking order validation
+        Order.setRestaurants(Restaurant.getRestaurantsFromRestServer(baseUrl));
+        List<Order> allOrders = RetrieveData.getData(baseUrl, "/orders", new TypeReference<>(){});
+        // order validation data
+        int invalidTotal = 0;
+        int invalidCvv = 0;
+        int invalidExpiryDate = 0;
+        int invalidCardNumber = 0;
+        int invalidPizzaCount = 0;
+        int invalidNotFound = 0;
+        int invalidMultipleSuppliers = 0;
+        int valid = 0;
+        for (Order o: allOrders)
+        {
+            switch (o.getOutcome())
+            {
+                case InvalidCardNumber -> invalidCardNumber += 1;
+                case InvalidTotal -> invalidTotal += 1;
+                case InvalidCvv -> invalidCvv += 1;
+                case InvalidExpiryDate -> invalidExpiryDate += 1;
+                case InvalidPizzaCount -> invalidPizzaCount += 1;
+                case InvalidPizzaCombinationMultipleSuppliers -> invalidMultipleSuppliers += 1;
+                case InvalidPizzaNotDefined -> invalidNotFound += 1;
+                default -> valid += 1;
+            }
+        }
+        // checks being performed
+        assertEquals(150, invalidCardNumber);
+        assertEquals(150, invalidExpiryDate);
+        assertEquals(150, invalidCvv);
+        assertEquals(150, invalidTotal);
+        assertEquals(150, invalidPizzaCount);
+        assertEquals(150, invalidNotFound);
+        assertEquals(150, invalidMultipleSuppliers);
+        assertEquals(6000, valid);
+    }
+
+    @Test
+    @DisplayName("Testing if order validation for REST server data for orders on a given date works as expected (1)")
+    void testOrderValidationForOrdersOnGivenDate1()
+    {
+        // set up data before checking order validation
+        Order.setRestaurants(Restaurant.getRestaurantsFromRestServer(baseUrl));
+        List<Order> allOrders = RetrieveData.getData(baseUrl, "/orders/2023-01-15", new TypeReference<>(){});
+        // order validation data
+        int invalidTotal = 0;
+        int invalidCvv = 0;
+        int invalidExpiryDate = 0;
+        int invalidCardNumber = 0;
+        int invalidPizzaCount = 0;
+        int invalidNotFound = 0;
+        int invalidMultipleSuppliers = 0;
+        int valid = 0;
+        for (Order o: allOrders)
+        {
+            switch (o.getOutcome())
+            {
+                case InvalidCardNumber -> invalidCardNumber += 1;
+                case InvalidTotal -> invalidTotal += 1;
+                case InvalidCvv -> invalidCvv += 1;
+                case InvalidExpiryDate -> invalidExpiryDate += 1;
+                case InvalidPizzaCount -> invalidPizzaCount += 1;
+                case InvalidPizzaCombinationMultipleSuppliers -> invalidMultipleSuppliers += 1;
+                case InvalidPizzaNotDefined -> invalidNotFound += 1;
+                default -> valid += 1;
+            }
+        }
+        // checks being performed
+        assertEquals(1, invalidCardNumber);
+        assertEquals(1, invalidExpiryDate);
+        assertEquals(1, invalidCvv);
+        assertEquals(1, invalidTotal);
+        assertEquals(1, invalidPizzaCount);
+        assertEquals(1, invalidNotFound);
+        assertEquals(1, invalidMultipleSuppliers);
+        assertEquals(40, valid);
+    }
+
+    @Test
+    @DisplayName("Testing if order validation for REST server data for orders on a given date works as expected (2)")
+    void testOrderValidationForOrdersOnGivenDate2()
+    {
+        // set up data before checking order validation
+        Order.setRestaurants(Restaurant.getRestaurantsFromRestServer(baseUrl));
+        List<Order> allOrders = RetrieveData.getData(baseUrl, "/orders/2023-02-02", new TypeReference<>(){});
+        // order validation data
+        int invalidTotal = 0;
+        int invalidCvv = 0;
+        int invalidExpiryDate = 0;
+        int invalidCardNumber = 0;
+        int invalidPizzaCount = 0;
+        int invalidNotFound = 0;
+        int invalidMultipleSuppliers = 0;
+        int valid = 0;
+        for (Order o: allOrders)
+        {
+            switch (o.getOutcome())
+            {
+                case InvalidCardNumber -> invalidCardNumber += 1;
+                case InvalidTotal -> invalidTotal += 1;
+                case InvalidCvv -> invalidCvv += 1;
+                case InvalidExpiryDate -> invalidExpiryDate += 1;
+                case InvalidPizzaCount -> invalidPizzaCount += 1;
+                case InvalidPizzaCombinationMultipleSuppliers -> invalidMultipleSuppliers += 1;
+                case InvalidPizzaNotDefined -> invalidNotFound += 1;
+                default -> valid += 1;
+            }
+        }
+        // checks being performed
+        assertEquals(1, invalidCardNumber);
+        assertEquals(1, invalidExpiryDate);
+        assertEquals(1, invalidCvv);
+        assertEquals(1, invalidTotal);
+        assertEquals(1, invalidPizzaCount);
+        assertEquals(1, invalidNotFound);
+        assertEquals(1, invalidMultipleSuppliers);
+        assertEquals(40, valid);
     }
 
 }
--- src/test/java/uk/ac/ed/inf/SystemTest.java
+++ src/test/java/uk/ac/ed/inf/SystemTest.java
@@ -1,9 +1,14 @@
 package uk.ac.ed.inf;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,10 +19,220 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SystemTest
 {
 
+    // =========================================================================
+    // ============================= CONSTRUCTOR ===============================
+    // =========================================================================
+
+    /**
+     * constructor method (used to also handle URL exceptions)
+     * @throws MalformedURLException error thrown when an invalid URL is used
+     */
+    public SystemTest() throws MalformedURLException
+    {
+
+    }
+
+    // =========================================================================
+    // ================================ TESTS ==================================
+    // =========================================================================
+
     @BeforeEach
     void displayTestName(TestInfo testInfo)
     {
         System.out.println(testInfo.getDisplayName());
+    }
+
+    // base URL for REST server
+    URL baseUrl = new URL("https://ilp-rest.azurewebsites.net");
+
+    @Test
+    @DisplayName("Testing if the whole PizzaDronz app works as expected (1)")
+    void testWholeSystem1()
+    {
+        // set the available restaurants field to all the restaurants from the validated URL
+        Restaurant[] restaurants = Restaurant.getRestaurantsFromRestServer(baseUrl);
+        Order.setRestaurants(restaurants);
+        // retrieve all available orders for the validated date
+        String extension = "/orders/2023-01-11";
+        List<Order> allOrders = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set up all the no-fly-zones
+        extension = "/noFlyZones";
+        List<NoFlyZone> allNoFlyZones = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set the base URL inside the LngLat class
+        // (for retrieving data for the central area)
+        LngLat.setBaseUrl(baseUrl);
+        // create the drone
+        Drone drone = new Drone();
+        // set the date to be the validated date
+        drone.setDateOfFlightPlan("2023-01-11");
+        // and run the flight planning algorithm for the given date
+        drone.planFlightPath(allOrders);
+
+        // check to see if order outcome has changed for delivered orders
+        int totalDelivered = 0;
+        for (Order order: Order.getValidOrders())
+        {
+            if (order.getOutcome().equals(OrderOutcome.Delivered))
+            {
+                totalDelivered += 1;
+            }
+        }
+        assertEquals(20, totalDelivered);
+
+        // TODO: implement checks to ensure appropriate files are created
+
+    }
+
+    @Test
+    @DisplayName("Testing if the whole PizzaDronz app works as expected (2)")
+    void testWholeSystem2()
+    {
+        // set the available restaurants field to all the restaurants from the validated URL
+        Restaurant[] restaurants = Restaurant.getRestaurantsFromRestServer(baseUrl);
+        Order.setRestaurants(restaurants);
+        // retrieve all available orders for the validated date
+        String extension = "/orders/2023-02-03";
+        List<Order> allOrders = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set up all the no-fly-zones
+        extension = "/noFlyZones";
+        List<NoFlyZone> allNoFlyZones = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set the base URL inside the LngLat class
+        // (for retrieving data for the central area)
+        LngLat.setBaseUrl(baseUrl);
+        // create the drone
+        Drone drone = new Drone();
+        // set the date to be the validated date
+        drone.setDateOfFlightPlan("2023-02-03");
+        // and run the flight planning algorithm for the given date
+        drone.planFlightPath(allOrders);
+
+        // check to see if order outcome has changed for delivered orders
+        int totalDelivered = 0;
+        for (Order order: Order.getValidOrders())
+        {
+            if (order.getOutcome().equals(OrderOutcome.Delivered))
+            {
+                totalDelivered += 1;
+            }
+        }
+        assertEquals(20, totalDelivered);
+
+        // TODO: implement checks to ensure appropriate files are created
+
+    }
+
+    @Test
+    @DisplayName("Testing if the whole PizzaDronz app works as expected (3)")
+    void testWholeSystem3()
+    {
+        // set the available restaurants field to all the restaurants from the validated URL
+        Restaurant[] restaurants = Restaurant.getRestaurantsFromRestServer(baseUrl);
+        Order.setRestaurants(restaurants);
+        // retrieve all available orders for the validated date
+        String extension = "/orders/2023-03-21";
+        List<Order> allOrders = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set up all the no-fly-zones
+        extension = "/noFlyZones";
+        List<NoFlyZone> allNoFlyZones = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set the base URL inside the LngLat class
+        // (for retrieving data for the central area)
+        LngLat.setBaseUrl(baseUrl);
+        // create the drone
+        Drone drone = new Drone();
+        // set the date to be the validated date
+        drone.setDateOfFlightPlan("2023-03-21");
+        // and run the flight planning algorithm for the given date
+        drone.planFlightPath(allOrders);
+
+        // check to see if order outcome has changed for delivered orders
+        int totalDelivered = 0;
+        for (Order order: Order.getValidOrders())
+        {
+            if (order.getOutcome().equals(OrderOutcome.Delivered))
+            {
+                totalDelivered += 1;
+            }
+        }
+        assertEquals(20, totalDelivered);
+
+        // TODO: implement checks to ensure appropriate files are created
+
+    }
+
+    @Test
+    @DisplayName("Testing if the whole PizzaDronz app works as expected (4)")
+    void testWholeSystem4()
+    {
+        // set the available restaurants field to all the restaurants from the validated URL
+        Restaurant[] restaurants = Restaurant.getRestaurantsFromRestServer(baseUrl);
+        Order.setRestaurants(restaurants);
+        // retrieve all available orders for the validated date
+        String extension = "/orders/2023-04-13";
+        List<Order> allOrders = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set up all the no-fly-zones
+        extension = "/noFlyZones";
+        List<NoFlyZone> allNoFlyZones = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set the base URL inside the LngLat class
+        // (for retrieving data for the central area)
+        LngLat.setBaseUrl(baseUrl);
+        // create the drone
+        Drone drone = new Drone();
+        // set the date to be the validated date
+        drone.setDateOfFlightPlan("2023-04-13");
+        // and run the flight planning algorithm for the given date
+        drone.planFlightPath(allOrders);
+
+        // check to see if order outcome has changed for delivered orders
+        int totalDelivered = 0;
+        for (Order order: Order.getValidOrders())
+        {
+            if (order.getOutcome().equals(OrderOutcome.Delivered))
+            {
+                totalDelivered += 1;
+            }
+        }
+        assertEquals(20, totalDelivered);
+
+        // TODO: implement checks to ensure appropriate files are created
+
+    }
+
+    @Test
+    @DisplayName("Testing if the whole PizzaDronz app works as expected (5)")
+    void testWholeSystem5()
+    {
+        // set the available restaurants field to all the restaurants from the validated URL
+        Restaurant[] restaurants = Restaurant.getRestaurantsFromRestServer(baseUrl);
+        Order.setRestaurants(restaurants);
+        // retrieve all available orders for the validated date
+        String extension = "/orders/2023-05-22";
+        List<Order> allOrders = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set up all the no-fly-zones
+        extension = "/noFlyZones";
+        List<NoFlyZone> allNoFlyZones = RetrieveData.getData(baseUrl, extension, new TypeReference<>(){});
+        // set the base URL inside the LngLat class
+        // (for retrieving data for the central area)
+        LngLat.setBaseUrl(baseUrl);
+        // create the drone
+        Drone drone = new Drone();
+        // set the date to be the validated date
+        drone.setDateOfFlightPlan("2023-05-22");
+        // and run the flight planning algorithm for the given date
+        drone.planFlightPath(allOrders);
+
+        // check to see if order outcome has changed for delivered orders
+        int totalDelivered = 0;
+        for (Order order: Order.getValidOrders())
+        {
+            if (order.getOutcome().equals(OrderOutcome.Delivered))
+            {
+                totalDelivered += 1;
+            }
+        }
+        assertEquals(20, totalDelivered);
+
+        // TODO: implement checks to ensure appropriate files are created
+
     }
 
 }
